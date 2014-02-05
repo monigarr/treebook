4f5adaf360c0e893f6c7aec505d06970435cef16
MONICA PETERS <monigarr@yahoo.com>
2014-02-04 20:12:37 -0500

    Install devise, add a user, and install devise views

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+  #setup or protected attributes 
+  #attr_accessible :email, :password, :password_confirmation, 
+  #	:remember_me, :first_name, :last_name, :profile_name
+
+end
